1171af2f63662948ec3f325aaa8d4d358614db73
Arnold Filip <arnold.filip@gmail.com>
2021-09-05 14:34:40 +0200

    Add static website

--- index.ts
+++ index.ts
@@ -14,6 +14,22 @@ const storageAccount = new storage.StorageAccount('qstssa', {
   kind: storage.Kind.StorageV2,
 })
 
+// Enable static website support
+const staticWebsite = new storage.StorageAccountStaticWebsite('staticWebsite', {
+  accountName: storageAccount.name,
+  resourceGroupName: resourceGroup.name,
+  indexDocument: 'index.html',
+})
+
+// Upload the file
+const indexHtml = new storage.Blob('index.html', {
+  resourceGroupName: resourceGroup.name,
+  accountName: storageAccount.name,
+  containerName: staticWebsite.containerName,
+  source: new pulumi.asset.FileAsset('index.html'),
+  contentType: 'text/html',
+})
+
 // Export the primary key of the Storage Account
 const storageAccountKeys = pulumi
   .all([resourceGroup.name, storageAccount.name])
@@ -21,3 +37,6 @@ const storageAccountKeys = pulumi
     storage.listStorageAccountKeys({ resourceGroupName, accountName }),
   )
 export const primaryStorageKey = storageAccountKeys.keys[0].value
+
+// Web endpoint to the website
+export const staticEndpoint = storageAccount.primaryEndpoints.web
